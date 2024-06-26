b6e97cafd0bf9678f17531e0f6b13a505e44a95b
Abjorny <117577552+Abjorny@users.noreply.github.com>
2024-06-26 18:29:04 +0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-tg = window.Telegram.WebApp;
-function cointiup(){
-let value =Number( document.getElementById('coin-my').textContent);
-    document.getElementById('coin-my').textContent = value+1;
-    console.log(value);
-    console.log(tg.initData)
-}
